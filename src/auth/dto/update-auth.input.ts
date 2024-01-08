8d4735005764549191c389564cd485902279c6e2
wojtekswietojanski <125148871+wojtekswietojanski@users.noreply.github.com>
2024-01-08 19:17:29 +0100

    All funcionalities are ready but need testing

--- src/auth/dto/update-auth.input.ts
+++
@@ -1,8 +0,0 @@
-import { SignUpInput } from './signup-input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
-
-@InputType()
-export class UpdateAuthInput extends PartialType(SignUpInput) {
-  @Field(() => Int)
-  id: number;
-}
